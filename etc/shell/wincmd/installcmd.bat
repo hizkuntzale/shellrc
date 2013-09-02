077e34b20ca242b3a01e48244a83f75fe2a9a368
Maxim Lihachev <envrm@yandex.ru>
2013-09-02 15:17:09 +0600

    Запись %APPDATA% в реестр без подстановки значения

--- etc/shell/wincmd/installcmd.bat
+++ etc/shell/wincmd/installcmd.bat
@@ -1,5 +1,5 @@
 :: Создан:  Пн 26 авг 2013 14:08:03
-:: Изменён: Чт 29 авг 2013 09:30:58
+:: Изменён: Пн 02 сен 2013 15:11:34
 
 @ECHO OFF
 
@@ -8,14 +8,20 @@
 ::
 
 
-if "%1" == "" (set rootDir=%APPDATA%\bin\win) else (set rootDir=%1)
+if "%1" == "" (
+	set rootDir=%APPDATA%\bin\win
+	set installDir="%%APPDATA%%\bin\win"
+) else (
+	set rootDir=%1
+	set installDir=%1
+)
 
 mkdir "%rootDir%"
 
 del /F "%rootDir%\cmdrc.bat"
 copy cmdrc.bat "%rootDir%\"
 
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d "\"%rootDir:\=\\%\\cmdrc.bat\""
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Command Processor" /v AutoRun /t REG_SZ /d "\"%installDir:\=\\%\\cmdrc.bat\""
 
 pause
 
