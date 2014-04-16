137e7ab91cf8c5ee0ae15d17714f33c9ddd63993
Maxim Lihachev <envrm@yandex.ru>
2014-04-16 23:53:39 +0600

    vimwiki: включена сортировка всех таблиц

--- vimwiki/style/js/sorttable.js
+++ vimwiki/style/js/sorttable.js
@@ -32,9 +32,9 @@ sorttable = {
     sorttable.DATE_RE = /^(\d\d?)[\/\.-](\d\d?)[\/\.-]((\d\d)?\d\d)$/;
 
     forEach(document.getElementsByTagName('table'), function(table) {
-      if (table.className.search(/\bsortable\b/) != -1) {
+      // if (table.className.search(/\bsortable\b/) != -1) {
         sorttable.makeSortable(table);
-      }
+      // }
     });
 
   },
